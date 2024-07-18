e560eb651cb455ce29ee6b77562e349b8e0d6012
Tracktor <tracktor@bk.ru>
2024-07-18 19:54:17 +0300

    Delete trash files

--- src/Components/Common/Button/Button.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-const Button = (props) => {
-    const { className, severity, type, label, onClick, isDisabled, } = props;
-    const defaultClassName = className ? `button ${className}` : 'button';
-    const buttonClassName = severity ? `${defaultClassName} button-${severity}` : `${defaultClassName} button-default`;
-    return (_jsx("button", { type: type === 'submit' ? 'submit' : 'button', className: buttonClassName, disabled: isDisabled, onClick: onClick, children: label }));
-};
-export default Button;
